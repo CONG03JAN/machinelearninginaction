233c74f2cd5c488b59ddd3243d0ebfe7b9d53668
CoffeeXC <pengc825@gmail.com>
2018-10-23 16:24:07 +0800

    添加 .gitignore 列表 添加 NaiveBayesian 实现

---
+++ NaiveBayesian/bayes.py
@@ -0,0 +1,41 @@
+#!/Users/coffeexc/anaconda2/envs/L3/bin/python
+# -*- coding:utf-8 -*-
+import numpy as np
+
+
+def loadDataSet():
+    postingList = [
+        ['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+        ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+        ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+        ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+        ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+        ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']
+    ]
+    classVec = [0, 1, 0, 1, 0, 1]
+    return postingList, classVec
+
+
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+
+def setOfWords2Vec(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print("the word: %s is not in my Vocabulary!" % word)
+    return returnVec
+
+
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    p0Num = np.zeros(numWords); p1Num = np.zeros(numWords)
+    
