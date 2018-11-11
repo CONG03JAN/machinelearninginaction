fa95797796cd3553df0249f3987c824bf91865b0
CoffeeXC <pengc825@gmail.com>
2018-11-11 19:34:25 +0800

    * 完成回归树和模型树的实现

---
+++ Tree/regTree.py
@@ -0,0 +1,220 @@
+#!/Users/coffeexc/anaconda2/envs/L3/bin/python
+# -*- coding:utf-8 -*-
+
+# 树回归的一般方法
+# 1. 数据收集：采用任意方法收集数据。
+# 2. 准备数据：需要数值型的数据，标称型数据应该映射成二值型数据。
+# 3. 分析数据：绘出数据的二维可视化显示结果，以字典方式生成树
+# 4. 训练算法：大部分时间都花费在叶节点树模型的构建上
+# 5. 测试算法：使用测试数据上的 R^2 值来分析模型的效果
+# 6. 使用算法：使用训练出的树做预测，预测结果还可以用来做很多事情
+
+import numpy as np
+from scipy import linalg
+
+
+def loadDataSet(fileName):
+    dataMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = []
+        for it in curLine:
+            fltLine.append(float(it))
+        dataMat.append(fltLine)
+    return dataMat
+
+
+def binSplitDataSet(dataSet, feature, value):
+    mat0 = dataSet[np.nonzero(dataSet[:, feature] > value)[0], :]
+    mat1 = dataSet[np.nonzero(dataSet[:, feature] <= value)[0], :]
+    return mat0, mat1
+
+
+def createTree(dataSet, leafType, errType, ops=(1, 4)):
+    feat, val = chooseBestSplit(dataSet, leafType, errType, ops)
+    if feat == None:
+        return val
+    retTree = {}
+    retTree['spInd'] = feat
+    retTree['spVal'] = val
+    lSet, rSet = binSplitDataSet(dataSet, feat, val)
+    retTree['left'] = createTree(lSet, leafType, errType, ops)
+    retTree['right'] = createTree(rSet, leafType, errType, ops)
+    return retTree
+
+
+# 回归树的切分函数
+def regLeaf(dataSet):
+    return np.mean(dataSet[:, -1])
+
+
+def regErr(dataSet):
+    return np.var(dataSet[:, -1]) * np.shape(dataSet)[0]
+
+
+def chooseBestSplit(dataSet, leafType, errType, ops):
+    tolS = ops[0]  # 容许的误差下降值
+    tolN = ops[1]  # 切分的最小样本数
+
+    # 如果所有的值相等则退出
+    if len(set(dataSet[:, -1].T.tolist()[0])) == 1:
+        return None, leafType(dataSet)
+
+    m, n = np.shape(dataSet)
+    S = errType(dataSet)
+    bestS = np.inf; bestIndex = 0; bestValue = 0
+
+    for featIndex in range(n - 1):
+        for splitVal in set(dataSet[:, featIndex].T.A.tolist()[0]):
+            mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
+            if (np.shape(mat0)[0] < tolN) or (np.shape(mat1)[0] < tolN):
+                continue
+            newS = errType(mat0) + errType(mat1)
+            if newS < bestS:
+                bestIndex = featIndex
+                bestValue = splitVal
+                bestS = newS
+
+    # 如果误差减小不大则退出
+    if (S - bestS) < tolS:
+        return None, leafType(dataSet)
+
+    mat0, mat1 = binSplitDataSet(dataSet, bestIndex, bestValue)
+
+    if (np.shape(mat0)[0] < tolN) or (np.shape(mat1)[0] < tolN):
+        return None, leafType(dataSet)
+
+    return bestIndex, bestValue
+
+
+# 后剪枝
+
+# 判断是否为树
+def isTree(obj):
+    return (type(obj).__name__ == 'dict')
+
+
+# 对子树进行塌陷处理
+def getMean(tree):
+    if isTree(tree['right']):
+        tree['right'] = getMean(tree['right'])
+    if isTree(tree['left']):
+        tree['left'] = getMean(tree['left'])
+    return (tree['left'] + tree['right']) / 2.0
+
+
+# 剪枝处理
+# tree: 待剪枝的树
+# testData: 剪枝所需的测试数据
+def prune(tree, testData):
+
+    # 没有测试数据则对数进行塌陷处理
+    if np.shape(testData)[0] == 0:
+        return getMean(tree)
+
+    lSet = []
+    rSet = []
+    if (isTree(tree['right']) or isTree(tree['left'])):
+        lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
+
+    if isTree(tree['left']):
+        tree['left'] = prune(tree['left'], lSet)
+
+    if isTree(tree['right']):
+        tree['right'] = prune(tree['right'], rSet)
+
+    if not isTree(tree['left']) and not isTree(tree['right']):
+        lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
+        errorNoMerge = sum(np.power(lSet[:, -1] - tree['left'], 2)) + sum(np.power(rSet[:, -1] - tree['right'], 2))
+        treeMean = (tree['left'] + tree['right']) / 2.0
+        errorMerge = sum(np.power(testData[:, -1] - treeMean, 2))
+        if errorMerge < errorNoMerge:
+            # print("merging")
+            return treeMean
+        else:
+            return tree
+    else:
+        return tree
+
+
+# 模型树
+def linearSolve(dataSet):
+    m, n = np.shape(dataSet)
+    X = np.mat(np.ones((m, n)))
+    Y = np.mat(np.ones((m, 1)))
+    X[:, 1:n] = dataSet[:, 0:n-1]
+    Y = dataSet[:, -1]
+    xTx = X.T * X
+    if linalg.det(xTx) == 0.0:
+        raise NameError("This matrix is singular, cannot do inverse,\ntry increasing the second value of ops")
+    ws = xTx.I * (X.T * Y)
+    return ws, X, Y
+
+
+def modelLeaf(dataSet):
+    ws, X, Y = linearSolve(dataSet)
+    return ws
+
+
+def modelErr(dataSet):
+    ws, X, Y = linearSolve(dataSet)
+    yHat = X * ws
+    return sum(np.power(Y - yHat, 2))
+
+
+# 利用树回归进行预测的代码
+def regTreeEval(model, inDat):
+    return float(model)
+
+
+def modelTreeEval(model, inDat):
+    n = np.shape(inDat)[1]
+    X = np.mat(np.ones((1, n + 1)))
+    X[:, 1:n+1] = inDat
+    return float(X * model)
+
+
+def treeForeCast(tree, inData, modelEval=regTreeEval):
+    if not isTree(tree):
+        return modelEval(tree, inData)
+    if inData[tree['spInd']] > tree['spVal']:
+        if isTree[tree['left']]:
+            return treeForeCast(tree['left'], inData, modelEval)
+        else:
+            return modelEval(tree['left'], inData)
+    else:
+        if isTree[tree['right']]:
+            return treeForeCast(tree['right'], inData, modelEval)
+        else:
+            return modelEval(tree['right'], inData)
+
+
+def createForeCast(tree, testData, modelEval=regTreeEval):
+    m = len(testData)
+    yHat = np.mat(np.zeros((m, 1)))
+    for i in range(m):
+        yHat[i, 0] = treeForeCast(tree, np.mat(testData[i]), modelEval)
+    return yHat
+
+
+if __name__ == '__main__':
+
+    myDat = loadDataSet('ex2.txt')
+    myMat = np.mat(myDat)
+    myTree = createTree(myMat, regLeaf, regErr, ops=(0, 1))
+    print(myTree)
+    myDatTest = loadDataSet('ex2test.txt')
+    myMatTest = np.mat(myDatTest)
+    myTree = prune(myTree, myMatTest)
+    print(myTree)
+
+
+
+
+
+
+
+
+
+
